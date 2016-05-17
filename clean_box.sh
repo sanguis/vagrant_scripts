43b17551b16413d09894f5dc90cec3e06bbe5576
Josh Beauregard <josh_beauregard@harvard.edu>
2016-05-17 16:15:40 -0400

    Adding new untested clean box

---
+++ clean_box.sh
@@ -0,0 +1,19 @@
+#!/usr/bash
+
+# Cleans a box in prep for creating a new box file. reduces cruft. 
+# may take a while to run.
+# Adapted from https://scotch.io/tutorials/how-to-create-a-vagrant-base-box-from-an-existing-one
+# run with:
+# `vagrant ssh -c "wget -qO- `
+
+ # Remove APT cache
+ sudo apt-get clean
+
+ # Then, “zero out” the drive (this is for Ubuntu):
+
+ sudo dd if=/dev/zero of=/EMPTY bs=1M
+ sudo rm -f /EMPTY
+
+ # Clear the Bash History and exit the VM:
+
+ cat /dev/null > ~/.bash_history && history -c && exit
